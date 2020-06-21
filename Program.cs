3135e60a41c24311fcb0c09ce260839dfa20963d
Matheus Khairallah <61334878+MatheusKhairallah@users.noreply.github.com>
2020-06-20 21:13:37 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Negativo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Escreva um número: ");
+            decimal numero=Convert.ToDecimal(Console.ReadLine());
+
+            Console.Clear();
+            
+            if(numero>=0)
+            Console.WriteLine("Você digitou um número positivo");
+            else
+            Console.WriteLine("Você digitou um número negativo");
+            
+            
+        }
+    }
+}
